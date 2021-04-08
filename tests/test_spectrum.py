711634a0a8678c2640c685f9d1507c9f31b7b972
Axel Runnholm <axel.runnholm@astro.su.se>
2021-04-08 17:24:29 +0200

    Add testfiles and update gitignore

---
+++ tests/test_spectrum.py
@@ -0,0 +1,33 @@
+from optispec import spectrum
+import numpy as np
+
+
+def test_read_fits():
+    """ Test the fits reading function
+    """
+    pass
+
+def test_read_ascii():
+    """
+    """
+    assert True
+
+def test_read_spectum():
+    """
+    """
+    assert True
+
+def test_spec_to_restframe():
+    """
+    """
+    assert True
+
+def test_convert_to_velocity():
+    """
+    """
+    assert True
+
+def test_shuffle_spectrum():
+    """
+    """
+    assert True
